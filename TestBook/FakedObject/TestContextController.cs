8d2e39dfbb6ae5eaca7b1c374296efecb4b323e3
monstrU <hp1999@mail.ru>
2014-11-16 17:53:26 +0300

    тесты

---
+++ TestBook/FakedObject/TestContextController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using Moq;
+
+namespace TestBook.FakedObject
+{
+    public class TestContextController : Controller
+    {
+        public TestContextController()
+        {
+            ControllerContext = (new Mock<ControllerContext>()).Object;
+        } 
+
+        public bool TestTryValidateModel(object model)
+        {
+            return TryValidateModel(model);
+        }
+    }
+}
---
+++ TestBook/Modeltest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Activities.Validation;
+using DomainModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestBook.FakedObject;
+
+namespace TestBook
+{
+    [TestClass]
+    public class ModelTest
+    {
+        [TestMethod]
+        public void ModelValidateTestOk()
+        {
+            var model = new MessageModel();
+            model.UpdateDate = DateTime.Now.AddDays(1);
+            model.Title = "title";
+            model.Body = "body";
+            TestContextController controller = new TestContextController();
+            var result= controller.TestTryValidateModel(model);
+            
+            Assert.IsTrue(result);
+
+            var state = controller.ModelState;
+            Assert.IsTrue(state.IsValid);
+            
+
+        }
+
+        [TestMethod]
+        public void ModelValidateTestFalse()
+        {
+            var model = new MessageModel();
+            model.UpdateDate = DateTime.Now.AddDays(-1);
+            model.Title = "title";
+            model.Body = "body";
+            TestContextController controller = new TestContextController();
+            var result = controller.TestTryValidateModel(model);
+            
+            Assert.IsFalse(result);
+            
+            var state = controller.ModelState;
+            Assert.IsFalse(state.IsValid);
+
+
+        }
+    }
+}
