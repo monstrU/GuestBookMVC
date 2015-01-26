d4a3eeb6d10cd6d68c78b6c84016fabd95e91e65
monstrU <hp1999@mail.ru>
2015-01-26 11:47:22 +0300

    тест на контроллер home

---
+++ GuestBook/ModelBinders/DecimalModelBinder.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.ModelBinding;
+using System.Web.Mvc;
+using IModelBinder = System.Web.Mvc.IModelBinder;
+using ModelBindingContext = System.Web.Mvc.ModelBindingContext;
+using ModelState = System.Web.Mvc.ModelState;
+
+
+namespace GuestBook
+{
+    public class DecimalModelBinder : IModelBinder
+    {
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            const string decSeparatorComma=",";
+            const string decSeparatorNeutral = ".";
+
+
+            var valueResult = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
+
+            var modelState = new ModelState { Value = valueResult };
+            object actualValue = null;
+            try
+            {
+                var strValue = valueResult.AttemptedValue;
+                var decSeparator = CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator;
+                if (decSeparator == decSeparatorComma)
+                {
+                    if (strValue.IndexOf(decSeparatorNeutral, System.StringComparison.Ordinal) >= 0)
+                    {
+                        strValue = strValue.Replace(decSeparatorNeutral, decSeparatorComma);
+                    }
+                }
+                else
+                {
+                    if (strValue.IndexOf(decSeparatorComma, System.StringComparison.Ordinal) >= 0)
+                    {
+                        strValue = strValue.Replace(decSeparatorComma, decSeparatorNeutral);
+                    }
+                }
+
+                actualValue = Convert.ToDecimal(strValue, CultureInfo.CurrentCulture);
+            }
+            catch (FormatException e)
+            {
+                modelState.Errors.Add(e);
+            }
+
+            bindingContext.ModelState.Add(bindingContext.ModelName, modelState);
+
+            return actualValue;
+        }
+    }
+}
---
+++ TestBook/HomeViewTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using DomainModel;
+using FacadeServices.Interfaces;
+using GuestBook.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace TestBook
+{
+    [TestClass]
+    public class HomeViewTests
+    {
+        [TestMethod]
+        public void IndexViewTest()
+        {
+            var service = new Mock<IGuestBookService>();
+            const int bookId = 1;
+            service.Setup(s => s.GetDefaultGuestBook(bookId)).Returns(new GuestBookModel()
+            {
+                GuestBookId = bookId,
+                GuestBookName = "Book name"
+            });
+            service.Setup(s2 => s2.LoadMessagesInBook(bookId)).Returns(new List<MessageModel>
+            {
+                new MessageModel() { MessageId = 1, Body = "message 1"},
+                new MessageModel() { MessageId = 2, Body = "message 2"}
+            });
+
+            var home = new HomeController(service.Object);
+            var result = home.Index();
+            Assert.IsNotNull(result);
+            
+        }
+    }
+}
